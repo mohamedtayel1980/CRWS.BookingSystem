71232e2ab8c8d31dc91e704b04e8a49b47e1e263
mohamed.tayel <moh.moh701@gmail.com>
2021-09-18 09:11:21 +0200

    Add project files.

---
+++ Contracts/BookingDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+   public class BookingDto
+    {
+        public int Id { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        public int BookedQuantity { get; set; }
+        public int ResourceId { get; set; }
+    }
+}
---
+++ Contracts/ResourceDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Contracts
+{
+    public class ResourceDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
