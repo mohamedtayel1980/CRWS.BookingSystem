aa5bd2b22d92709ebfaa791a7a34be4a9525a18a
mohamed.tayel <moh.moh701@gmail.com>
2021-09-21 17:43:10 +0200

    add swager

---
+++ Presentation/Controllers/ResourceController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/resource/")]
+    public class ResourceController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+        public ResourceController(IServiceManager serviceManager)
+            => _serviceManager = serviceManager;
+
+        [HttpGet]
+        public IActionResult GetResource()
+        {
+            var resourcesDto =  _serviceManager.ResourceService.ResourceQueryService
+                .GetAllResources();
+
+            return Ok(resourcesDto);
+        }
+    }
+}
